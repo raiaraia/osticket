d3cef7b03d1ba48f6360f5b06cd1300de9396204
raiaraia <75408485+raiaraia@users.noreply.github.com>
2021-02-05 11:15:55 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -206,6 +206,8 @@ extends SessionBackend {
         if (!isset($this->data))
             $this->data = new SessionData(['session_id' => $id]);
 
+        catch (DoesNotExist $e) {
+        $this->data = new SessionData(['session_id' => $id]);
         $this->data->session_data = "";
         $this->data->session_expire =
             SqlFunction::NOW()->plus(SqlInterval::SECOND($ttl));
